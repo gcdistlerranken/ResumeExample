96c62745ada4953060d172b74b5db1671e2f9929
grace_distler <grace_distler@insideranken.org>
2020-04-01 13:15:51 -0500

    Second Commit. Model package and Model classes created.

---
+++ app/src/main/java/com/google/distlergrace/resumeexample/model/ModelProject.java
@@ -0,0 +1,55 @@
+package com.google.distlergrace.resumeexample.model;
+
+public class ModelProject
+{
+    public String projectName;
+    public String projectType;
+    public String projectDescription;
+    public String projectImagePath;
+    public String projectLandinPage;
+
+    public String getProjectName()
+    {
+        return projectName;
+    }
+    public void setProjectName(String projectName)
+    {
+        this.projectName = projectName;
+    }
+
+    public String getProjectType()
+    {
+        return projectType;
+    }
+    public void setProjectType(String projectType)
+    {
+        this.projectType = projectType;
+    }
+
+    public String getProjectDescription()
+    {
+        return projectDescription;
+    }
+    public void setProjectDescription(String projectDescription)
+    {
+        this.projectDescription = projectDescription;
+    }
+
+    public String getProjectImagePath()
+    {
+        return projectImagePath;
+    }
+    public void setProjectImagePath(String projectImagePath)
+    {
+        this.projectImagePath = projectImagePath;
+    }
+
+    public String getProjectLandinPage()
+    {
+        return projectLandinPage;
+    }
+    public void setProjectLandinPage(String projectLandinPage)
+    {
+        this.projectLandinPage = projectLandinPage;
+    }
+}
